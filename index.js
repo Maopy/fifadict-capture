d49ebf68ca352abbc4a781dcb0b9b796ea79b6ed
Maopy <maopy@51haoyayi.com>
2016-07-24 00:19:01 +0800

    repeat suc

--- index.js
+++ index.js
@@ -3,21 +3,24 @@
 var browser = require('casper').create()
 var start = new Date()
 var acceleration
+var i = 1
 
-browser.start()
-
-browser.thenOpen('http://cn.fifaaddict.com/fo3player.php?id=93190043')
-
-browser.then(function () {
-  acceleration = browser.evaluate(function () {
-    return document.getElementsByClassName('acceleration')[0].innerText.trim()
+browser.start().repeat(3, function () {
+  this.echo(new Date() - start + 'start' + i)
+  i++
+  browser.thenOpen('http://cn.fifaaddict.com/fo3player.php?id=93190043')
+  
+  browser.then(function () {
+    acceleration = browser.evaluate(function () {
+      return document.getElementsByClassName('acceleration')[0].innerText.trim()
+    })
   })
-})
-
-browser.then(function () {
-  this.echo(acceleration)
-  this.echo(new Date() - start + 'ms')
-  browser.exit()
+  
+  browser.then(function () {
+    this.echo(acceleration)
+    this.echo(new Date() - start + 'ms')
+    // browser.exit()
+  })
 })
 
 browser.run()
