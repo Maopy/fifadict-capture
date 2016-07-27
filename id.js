571a7b63400c0cc1951f6c595daf25220ee4a5c3
Maopy <maopy@51haoyayi.com>
2016-07-27 11:40:55 +0800

    id

--- id.js
+++ id.js
@@ -1,19 +1,62 @@
 'use strict'
 
+const fs = require('fs')
 const fetch = require('node-fetch')
 const cheerio = require('cheerio')
+const Promise = require('bluebird')
 
-module.exports = (league, club) => {
-  return fetch(`http://cn.fifaaddict.com/fo3db.php?q=player&season=2006,2007,2008,2009,2010,2011&league=liga-bbva&club=Real-Madrid`)
+const leagueDB = require('./league.json')
+let idList = {}
+let errorList = []
+let hundredList = []
+
+let fetchId = (league, club) => {
+  return fetch(`http://cn.fifaaddict.com/fo3db.php?q=player&league=${league}&club=${club}`, {
+    timeout: 1000 * 10
+  })
     .then((res) => {
       return res.text()
     })
     .then((body) => {
       let $ = cheerio.load(body)
-      let idList = []
-      $('.player-row').each((i, p) => {
-        idList.push($(p).attr('id').substring(9))
+      let $pr = $('.player-row')
+      $pr.each((i, p) => {
+        idList[league].push($(p).attr('id').substring(9))
       })
-      return idList
+      console.log(`${league} ${club} success ${$pr.length}`)
+      if ($pr.length === 100) {
+        hundredList.push(`${league} ${club}`)
+      }
+      return idList[league]
+    })
+    .catch((err) => {
+      // console.log(err)
+      console.error(`${league} ${club} ERROR!!!`)
+      errorList.push(`${league} ${club}`)
     })
 }
+
+let fetchDB = () => {
+  Promise.mapSeries(leagueDB, (l) => {
+    idList[l.name] = []
+    return Promise.map(l.clubs, (c) => {
+      return fetchId(l.name, c.name)
+    })
+      .then((res) => {
+        fs.writeFileSync(`id/${l.name}_id.json`, JSON.stringify(idList[l.name]))
+        console.log(`${l.name} ${idList[l.name].length}`)
+      })
+  })
+    .then(() => {
+      console.log(errorList)
+      console.log(hundredList)
+    })
+}
+
+fetchDB()
+
+// fetchLeague('austrian-bundesliga')
+
+// console.log(leagueDB.length)
+
+// fetchId(, 'FK-Austria-Wien')
